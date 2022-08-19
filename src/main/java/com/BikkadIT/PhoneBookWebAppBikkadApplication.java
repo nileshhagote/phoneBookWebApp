7fa53fa9a1d31e3dd6b76722187e0630174fc35a
nileshhagote <nilesh.hagote8275@gmail.com>
2022-08-19 11:43:47 +0530

    This is phone book web app

---
+++ src/main/java/com/BikkadIT/PhoneBookWebAppBikkadApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookWebAppBikkadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneBookWebAppBikkadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BikkadIT/PhoneBookWebAppBikkadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookWebAppBikkadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
